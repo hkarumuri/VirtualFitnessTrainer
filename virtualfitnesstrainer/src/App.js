c0e6822929521ac52e213331349e4b197976334e
Harish Karumuri <harishskarumuri@gmail.com>
2021-03-27 22:07:54 -0400

    firebase configuration

--- virtualfitnesstrainer/src/App.js
+++ virtualfitnesstrainer/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+import { useCollectionState } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAMHQVMCtA5pVOqS0EWGPSmeNMwN4yu-hA",
+    authDomain: "virtual-fitness-trainer.firebaseapp.com",
+    projectId: "virtual-fitness-trainer",
+    storageBucket: "virtual-fitness-trainer.appspot.com",
+    messagingSenderId: "559528621216",
+    appId: "1:559528621216:web:17f402868d9b817c9845c5",
+    measurementId: "G-Z3D207RE86"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
